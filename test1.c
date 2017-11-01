34ed0a746f09b831e42de3dbf8decffc80e4f6dc
zhangbin-eos <zhangbin.eos@foxmail.com>
2017-11-01 05:40:49 -0400

    rm test1 ;vi test1.c

---
+++ test1.c
@@ -0,0 +1,12 @@
+/*================================================================
+*   Copyright (C) 2017 Sangfor Ltd. All rights reserved.
+*   
+*   filename      :test1.c
+*   autor         :ZhangBin
+*   createdate    :2017/11/01
+*   descripte     :
+*
+================================================================*/
+
+
+#include "test1.h"
